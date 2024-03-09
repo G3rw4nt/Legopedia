651555746511d76c2438b623660d02b1ea0ff74c
Krzysztof Dereszyński <krzysztof.dereszynski@gmail.com>
2024-03-09 01:45:32 +0100

    database connection

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import psycopg2
+import os
 
 app = Flask(__name__)
-
+print(os.getenv('DATABASE'))
+conn = psycopg2.connect(
+    host=os.getenv('HOST'),
+    user=os.getenv('USER'),
+    password=os.getenv('PASSWORD'),
+    port=os.getenv('PORT')
+)
 
 @app.route('/')
 def hello_world():  # put application's code here
