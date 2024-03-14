18776838205d15fd12327ac452d0fc290bb5b465
Julia Bielecka <juliabiel25@gmail.com>
2024-03-14 12:48:01 +0100

    switch to data grid component

--- legopedia-frontend/src/components/TableSection.jsx
+++ legopedia-frontend/src/components/TableSection.jsx
@@ -1,45 +1,125 @@
 import "./TableSection.css";
 
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+
 import { useEffect, useState } from "react";
 
-import {api} from '../api';
+import { api } from "../api";
 
 const TableSection = () => {
-    const [option, setOption] = useState("parts");
-    const [parts, setParts] = useState([]);
-    const [sets, setSets] = useState([]);
+  const [option, setOption] = useState("parts");
+  const [parts, setParts] = useState([]);
+  const [sets, setSets] = useState([]);
 
-    async function fetchParts () {
-        try {
-            const fetched = await api.get('/parts')
-            setParts(fetched.data)
-        } catch (err) {
-            console.error('fetchParts error: ', err)
-        }
+  async function fetchParts() {
+    try {
+      const fetched = await api.get("/parts");
+      setParts(fetched.data);
+    } catch (err) {
+      console.error("fetchParts error: ", err);
     }
-    
-    async function fetchSets () {
-        try {
-            const fetched = await api.get('/sets')
-            setSets(fetched.data)
-        } catch (err) {
-            console.error('fetchParts error: ', err)
-        }
-    }
+  }
 
-    useEffect(() => {
-        console.log('==== RELOAD ====')
-        fetchParts();
-        fetchSets();
-    }, []);
-    
-    return (
-        <div className="table-container">
-            <div className="table-buttons">
-                <button onClick={() => setOption('parts')}>PARTS</button>
-                <button onClick={() => setOption('sets')}>SETS</button>
-            </div>
-            <table className="table">
+  async function fetchSets() {
+    try {
+      const fetched = await api.get("/sets");
+      setSets(fetched.data);
+    } catch (err) {
+      console.error("fetchParts error: ", err);
+    }
+  }
+
+  useEffect(() => {
+    console.log("==== RELOAD ====");
+    fetchParts();
+    fetchSets();
+  }, []);
+
+  const columnsParts = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "part_num",
+      headerName: "Part Number",
+      type: "number",
+      width: 150,
+      editable: true
+    },
+    {
+      field: "name",
+      headerName: "Part Name",
+      width: 150,
+      editable: true
+    },
+    {
+      field: "part_cat_id",
+      headerName: "Category",
+      width: 110,
+      editable: true
+    }
+  ];
+
+  const columnsSets = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "set_num",
+      headerName: "Set Number",
+      width: 150,
+      editable: true
+    },
+    {
+      field: "name",
+      headerName: "Set Name",
+      width: 150,
+      editable: true
+    },
+    {
+      field: "year",
+      headerName: "Year",
+      type: "number",
+      width: 110,
+      editable: true
+    },
+    {
+      field: "num_parts",
+      headerName: "Number of parts",
+      type: "number",
+      width: 110,
+      editable: true
+    },
+    {
+      field: "theme_id",
+      headerName: "Theme",
+      type: "number",
+      width: 110,
+      editable: true
+    }
+  ];
+
+  return (
+    <div className="table-container">
+      <div className="table-buttons">
+        <button onClick={() => setOption("parts")}>PARTS</button>
+        <button onClick={() => setOption("sets")}>SETS</button>
+      </div>
+      <Box sx={{ height: 400, width: "100%" }}>
+        <DataGrid
+          style={{ color: "#fff" }}
+          rows={option === "parts" ? parts : sets}
+          columns={option === "parts" ? columnsParts : columnsSets}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5
+              }
+            }
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+      {/* <table className="table">
                 <thead>
                     {option === "parts" ? (
                         <tr>
@@ -64,8 +144,7 @@ const TableSection = () => {
                                 <td>{part.part_num}</td>
                                 <td>{part.name}</td>
                                 <td>{part.part_cat_id}</td>
-                                {/* zamienić na nazwę kategorii - najlepiej chyba na backu */}
-                                {/* <td>{part.category}</td> */}
+                                
                             </tr>
                         ))
                         : sets.map((set) => (
@@ -78,9 +157,9 @@ const TableSection = () => {
                             </tr>
                     ))}
                 </tbody>
-            </table>
-        </div>
-    );
+            </table> */}
+    </div>
+  );
 };
 
 export default TableSection;
